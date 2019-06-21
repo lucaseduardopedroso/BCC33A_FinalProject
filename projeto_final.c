954f9b6669528930160ae7262d9a3c4558c8a6f6
Gabriel Hamilton <gabrielmendes999@gmail.com>
2019-06-21 15:49:08 -0300

    Funções

---
+++ projeto_final.c
@@ -0,0 +1,20 @@
+// Universidade Tecnologica Federal do Paraná
+// Bacharelado em Ciencia da Computacao - Estrutura de Dados 2
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+isMaxHeap();
+isMinHeap();
+isBST();
+isAVL();
+isComplete();
+lowestValue();
+highestValue();
+mirrorTree();
+areCousins();
+isSUM();
+
+int main(){
+
+  };
